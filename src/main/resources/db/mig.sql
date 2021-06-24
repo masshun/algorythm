877f1184c9f6160cb9e7d2bdbe2b39489dfdd61c
松下俊 <shunmatsu753@gmail.com>
2021-06-24 11:15:20 +0900

    actions test

--- src/main/resources/db/mig.sql
+++ src/main/resources/db/mig.sql
@@ -3,5 +3,5 @@
 -- hgggっっgfffffff
 -- delete from t_cropffffffffhhhhjhjjkhjkhghghgg
 --DELETE FROM a_hoge;ffffhhj
---ALTER TABLE hogehogehoggggg
+--ALTER TABLE hogehogehoggggghige
 SELECT * FROM m_crop WHERE t_id IS NOT NULL;
